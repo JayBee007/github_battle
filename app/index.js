8df5e9f6ec48f3d8365b86c7685a4f7e0df736f7
Jay <jaybee.codes@gmail.com>
2017-05-09 12:44:41 +0300

    move app.js to seperate component folder

--- app/index.js
+++ app/index.js
@@ -1,13 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import './index.css';
 
-class App extends Component {
-    render(){
-        return (
-            <h1>Hello React!</h1>
-        )
-    }
-}
 
 ReactDOM.render (<App />, document.getElementById("app"));
