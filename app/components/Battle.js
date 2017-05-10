dc270e80b14f25ebd402502bfaceccbb3b7f5a80
Jay <jaybee.codes@gmail.com>
2017-05-10 22:08:40 +0300

    battle component ready, need to add react-router

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,68 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+
+class PlayerInput extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            username:''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(e){
+        let value = e.target.value;
+
+        this.setState({
+            username: value
+        })  
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.onSubimt(
+            this.props.id,
+            this.state.username
+        )
+    }
+    render(){
+        return(
+            <form onSubmit = {this.handleSubmit} className = "column">
+                <label className = 'header' htmlFor = 'username'>
+                    {this.props.label}
+                </label>
+                <input 
+                    id ='username'
+                    placeholder = 'Please enter GitHub Username'
+                    type = 'text'
+                    autoComplete = 'off'
+                    value = {this.state.username}
+                    onChange = {this.handleChange}
+                />
+                <button>
+                    className = 'button'
+                    type = 'submit'
+                    disabled = {!this.state.username}
+                        Submit
+                </button>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubimt: PropTypes.func.isRequired
+}
+
+
+
+
+
+
+
+
 
 class Battle extends Component {
     constructor(props){
@@ -19,9 +83,20 @@ class Battle extends Component {
         this.setState(newState);
     }
     render(){
+        let {playerOneName, playerTwoName} = this.state;
         return(
-            <div>
-                Battle!
+            <div className="row">
+                {!playerOneName &&
+                <PlayerInput
+                id= 'playerOne'
+                label = 'Player One'
+                onSubimt = {this.handleSubmit} />}
+
+                {!playerTwoName &&
+                <PlayerInput
+                id= 'playerOne'
+                label = 'Player Two'
+                onSubimt = {this.handleSubmit} />}
             </div>
         )
     }
