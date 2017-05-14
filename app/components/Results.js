e08b80420aaf846a60bc6651a66544abacf8bad9
Jay <jaybee.codes@gmail.com>
2017-05-14 19:15:59 +0300

    install query-string

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react'
+import queryString from 'query-string'
 
 class Results extends Component {
     render(){
+        let players = queryString.parse(this.props.location.search)
+        console.log(players);
         return(
             <div>Results</div>
         )
