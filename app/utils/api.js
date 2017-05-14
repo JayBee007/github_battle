4898db87b2e52d604f298680ea5334a9ed2f91d8
Jay <jaybee.codes@gmail.com>
2017-05-14 18:11:02 +0300

    create config.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
+import {ID, SECRET} from './config'
+
+let params = `?client_id=${ID}&client_secret=${SECRET}`
+
+
+
 
 export default {
+    battle: function(players) {
+
+    },
+
     fetchPopularRepos: function(language) {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
